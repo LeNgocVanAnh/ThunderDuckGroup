80d0b466f495e4d989c84336735cebaf9f3c0fda
LeNgocVanAnh <anhle161108@gmail.com>
2018-10-26 21:57:04 +0700

    khoi tao du an

---
+++ ThunderDuckGroup/Controllers/ThunderDuckConstruction/HomeConstructionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThunderDuckGroup.Controllers.ThunderDuckConstruction
+{
+    public class HomeConstructionController : Controller
+    {
+        // GET: HomeConstruction
+        public ActionResult IndexConstruction()
+        {
+            return View();
+        }
+    }
+}
