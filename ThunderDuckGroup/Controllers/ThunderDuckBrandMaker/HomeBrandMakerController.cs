76d459a5e389ac110c7c527f19104ff23a7e53ea
LeNgocVanAnh <anhle161108@gmail.com>
2018-10-27 00:56:40 +0700

    update project

--- ThunderDuckGroup/Controllers/ThunderDuckBrandMaker/HomeBrandMakerController.cs
+++ ThunderDuckGroup/Controllers/ThunderDuckBrandMaker/HomeBrandMakerController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ThunderDuckGroup.Models;
 namespace ThunderDuckGroup.Controllers.ThunderDuckBrandMaker
 {
     public class HomeBrandMakerController : Controller
     {
+        ThunderDuckEntities db = new ThunderDuckEntities();
         // GET: HomeBrandMaker
         public ActionResult IndexBrandMaker()
         {
+            List<HomeMaster> lst = new List<HomeMaster>();
+            HomeMaster home = new HomeMaster();
+            var story = db.Td_Story.Where(st => st.id == 1);
+            var tour = db.Td_Tours.Where(st => st.id == 1);
+            var events = db.Td_Events.Where(st => st.id == 1);
+            var image = db.Td_Images.Where(st => st.id == 1);
+            var newss = db.Td_News.Where(st => st.id == 1);
+            var contact = db.Td_Contact.Where(st => st.id == 1);
+            home.sto = story;
+            home.tou = tour;
+            home.eve = events;
+            home.img = image;
+            home.news = newss;
+            home.con = contact;
+            lst.Add(home);
             return View();
         }
     }
---
+++ ThunderDuckGroup/Models/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ThunderDuckGroup.Models;
+
+namespace ThunderDuckGroup.Models
+{
+    public class HomeMaster
+    {
+        public IQueryable<Td_Story> sto { get; set; }
+        public IQueryable<Td_Tours> tou { get; set; }
+        public IQueryable<Td_Events> eve { get; set; }
+        public IQueryable<Td_Images> img { get; set; }
+        public IQueryable<Td_Members> mem { get; set; }
+        public IQueryable<Td_News> news { get; set; }
+        public IQueryable<Td_Contact> con { get; set; }
+    }
+}
